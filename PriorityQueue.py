11c9eb44ff4e4d79e785b2d5d3a42cf876e5e12d
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-13 16:27:22 +0800

    "Setting the variable message with enqueue with priority"

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -5,3 +5,7 @@ IMPORTANT = 2
 NEUTRAL = 1
 
 messages = PriorityQueue
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station turned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")

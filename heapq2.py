f28f3c363f5765811bbf63e9bc08ef2b9654f55b
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-13 16:09:26 +0800

    "using heappop in the list"

---
+++ heapq2.py
@@ -0,0 +1,11 @@
+from heapq import heappush
+from heapq import heappop
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+heappop(fruits)
+
+print(fruits)

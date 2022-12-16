c3324c55780a355918efd18c5cb451cee9a55124
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 10:35:01 +0800

    "print using for loop"

--- graph8.py
+++ graph8.py
@@ -11,3 +11,7 @@ def sort_by(neighbors, strategy):
 
 def by_distance(weights):
     return float(weights["distance"])
+
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")

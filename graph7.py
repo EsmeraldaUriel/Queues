197645581d516385e482ad3e4e8d52db314985ce
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 10:32:22 +0800

    "copying graph6 and changing the print element "

---
+++ graph7.py
@@ -0,0 +1,7 @@
+from graph4 import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
+

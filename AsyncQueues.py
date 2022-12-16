74c61814f9ece2ee1904d1313203201c4085a4a0
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 16:58:37 +0800

    "async "

--- AsyncQueues.py
+++ AsyncQueues.py
@@ -65,3 +65,34 @@ async def worker(worker_id, session, queue, links, max_depth):
             queue.task_done()
 
 
+async def main(args):
+    session = aiohttp.ClientSession()
+    try:
+        links = Counter()
+        queue = asyncio.Queue()
+        tasks = [
+            asyncio.create_task(
+                worker(
+                    f"Worker-{i + 1}",
+                    session,
+                    queue,
+                    links,
+                    args.max_depth,
+                )
+            )
+            for i in range(args.num_workers)
+        ]
+
+        await queue.put(Job(args.url))
+        await queue.join()
+
+        for task in tasks:
+            task.cancel()
+
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+        display(links)
+    finally:
+        await session.close()
+
+

79ff8693f207f0243c880621b8af4da05d742417
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 10:49:17 +0800

    "async def fetch html "

--- AsyncQueues.py
+++ AsyncQueues.py
@@ -1,6 +1,8 @@
 import argparse
 import asyncio
 from collections import Counter
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
 import aiohttp
 
@@ -17,3 +19,13 @@ async def main(args):
 def display(links):
     for url, count in links.most_common():
         print(f"{count:>3} {url}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))
+
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()

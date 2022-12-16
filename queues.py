1393bcdff1a952663692f65f5fdec5328693684f
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 09:21:33 +0800

    "changing the whole file by copying queues4"

--- queues.py
+++ queues.py
@@ -1,15 +1,17 @@
-class IterableMixin:
+from collections import deque
+from heapq import heappop, heappush
+
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
     def __len__(self):
         return len(self._elements)
 
     def __iter__(self):
         while len(self) > 0:
             yield self.dequeue()
-
-
-class Queue(IterableMixin):
-    def __init__(self, *elements):
-        self._elements = deque(elements)
 
     def enqueue(self, element):
         self._elements.append(element)
@@ -23,44 +25,12 @@ class Stack(Queue):
         return self._elements.pop()
 
 
-class PriorityQueue(IterableMixin):
+class PriorityQueue:
     def __init__(self):
         self._elements = []
-        self._counter = count()
 
     def enqueue_with_priority(self, priority, value):
-        element = (-priority, next(self._counter), value)
-        heappush(self._elements, element)
+        heappush(self._elements, (-priority, value))
 
     def dequeue(self):
-        return heappop(self._elements)[-1]
-
-
-@dataclass(order=True)
-class Element:
-    priority: float
-    count: int
-    value: Any
-
-
-class MutableMinHeap(IterableMixin):
-    def __init__(self):
-        super().__init__()
-        self._elements_by_value = {}
-        self._elements = []
-        self._counter = count()
-
-    def __setitem__(self, unique_value, priority):
-        if unique_value in self._elements_by_value:
-            self._elements_by_value[unique_value].priority = priority
-            heapify(self._elements)
-        else:
-            element = Element(priority, next(self._counter), unique_value)
-            self._elements_by_value[unique_value] = element
-            heappush(self._elements, element)
-
-    def __getitem__(self, unique_value):
-        return self._elements_by_value[unique_value].priority
-
-    def dequeue(self):
-        return heappop(self._elements).value
+        return heappop(self._elements)[1]

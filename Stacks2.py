35d5dbdb1cd32f5b7fad5a456b33be7bec804a30
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 09:45:42 +0800

    "modifying print elements"

--- Stacks2.py
+++ Stacks2.py
@@ -3,7 +3,9 @@ lifo = []
 lifo.append("1st")
 lifo.append("2nd")
 lifo.append("3rd")
+# You add 3 input in empty list
 
 lifo.pop()
 
-print(lifo)
+print("The remaining elements: ", lifo)
+# You can see that the last element is removed

e97278ca1bc6aefe599f0a693c4a5443fd478809
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 09:38:56 +0800

    "modifying print elements"

--- FIFO.py
+++ FIFO.py
@@ -1,9 +1,9 @@
 from queues2 import Queue
 
-fifo = Queue("1st", "2nd", "3rd")
-print("In line: ", len(fifo))
+fifo = Queue("Person 1", "Person 2", "Person 3")
+print("In line: ", len(fifo), "Person/s")
 
 for element in fifo:
     print(element)
 
-print("In line: ", len(fifo))
+print("In line: ", len(fifo), "Person/s")
--- Stacks.py
+++ Stacks.py
@@ -2,4 +2,4 @@ from queues import Stack
 
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
-    print(element)
+    print("Your Current Place: ",element)

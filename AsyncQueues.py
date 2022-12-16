d999eb78823e93b11522834c840d88bdd1821ee2
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 10:43:29 +0800

    "def display "

--- AsyncQueues.py
+++ AsyncQueues.py
@@ -4,6 +4,7 @@ from collections import Counter
 
 import aiohttp
 
+
 async def main(args):
     session = aiohttp.ClientSession()
     try:
@@ -11,3 +12,8 @@ async def main(args):
         display(links)
     finally:
         await session.close()
+
+
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")

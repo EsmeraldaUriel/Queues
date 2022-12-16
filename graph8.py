efaaa5684b566e33f9d9d89fe6d426eb0994ca97
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 10:34:27 +0800

    "vdef func for distance"

--- graph8.py
+++ graph8.py
@@ -7,3 +7,7 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 def sort_by(neighbors, strategy):
     return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+
+def by_distance(weights):
+    return float(weights["distance"])

a80dc3112ce739df9b0b057fd6d9527ac81b3b9a
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2022-12-16 09:17:33 +0800

    "setting priority and value"

--- queues4.py
+++ queues4.py
@@ -29,3 +29,5 @@ class PriorityQueue:
     def __init__(self):
         self._elements = []
 
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
